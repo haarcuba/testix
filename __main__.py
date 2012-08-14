15a75d1201b0adc0314ee75f39a65fb1cae73fbf
Yoav Kleinberger <haarcuba@gmail.com>
2012-08-15 00:23:56 +0300

    orderly argument parsing for the executable

--- __main__.py
+++ __main__.py
@@ -1,5 +1,10 @@
 from testix import runner
-import sys
+import argparse
 
 if __name__ == '__main__':
-	runner.Runner( sys.argv[ 1: ] )
+	description = 	"testix - a Python unit test framework" \
+					"this is free software, available under the GNU General Public License version 3.0"
+	parser = argparse.ArgumentParser( description = description )
+	parser.add_argument( 'tests', nargs = '+', metavar = 'TEST', help = 'each test should be a python file implementing a test suite' )
+	arguments = parser.parse_args()
+	runner.Runner( arguments.tests )
