e84b5659584f9098a950f218fba594aec48560b3
Yoav Kleinberger <haarcuba@gmail.com>
2012-08-15 01:59:34 +0300

    refactoring

--- __main__.py
+++ __main__.py
@@ -3,9 +3,12 @@ import argparse
 import sys
 import os
 
-if __name__ == '__main__':
+def letSubprocessesInheritEggNamespace():
 	egg = os.path.realpath( sys.path[ 0 ] )
 	os.environ[ 'PYTHONPATH' ] = '%s:%s' % ( egg, os.environ[ 'PYTHONPATH' ] )
+
+if __name__ == '__main__':
+	letSubprocessesInheritEggNamespace()
 	description = 	"testix - a Python unit test framework" \
 					"this is free software, available under the GNU General Public License version 3.0"
 	parser = argparse.ArgumentParser( description = description )
