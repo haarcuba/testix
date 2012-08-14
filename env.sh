23f74c9fa9fcf2d67ae4e637b863bbd5ae152e37
Yoav Kleinberger <haarcuba@gmail.com>
2012-08-14 20:27:19 +0300

    work area indication

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@ export TOP=$(pwd)
 export PYTHONPATH=$TOP
 
 _indicateWorkArea() {
-	prompt='(testis) \W$ '
+	prompt='(testix) \W$ '
 	color='\[\e[34;1m\]'
 	resetColor='\[\e[0m\]'
 	export PS1="$color$prompt$resetColor"
