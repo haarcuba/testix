0f444e7ecc89d50e4588d9d118ca36d58616d1eb
Yoav Kleinberger <haarcuba@gmail.com>
2012-09-12 00:01:46 +0300

    make egg script - in case jam isn't installed

---
+++ makeegg.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+_createVersion()
+{
+	 gitHash=`git log | head -1 | awk '{ print $2; }'`
+	 echo "VERSION = '$gitHash'" > version.py 
+}
+
+_compileAllPythons() 
+{
+	for name in $(find testix -name '*py') ; do
+		echo compiling $name
+		python -c "import compiler ; compiler.compileFile( '$name' )" || exit -1
+	done
+}
+
+_zipAllPYC()
+{
+	zip -r testix.egg version.py __main__.py $(find testix -name '*pyc' | grep -v startertests)
+}
+
+_resetVersion()
+{
+	echo "VERSION = 'development'" > version.py
+}
+
+_main()
+{
+	rm -f testix.egg && \
+	_createVersion && \
+	_compileAllPythons &&\
+	_zipAllPYC &&\
+	_resetVersion
+}
+
+_error()
+{
+	echo ERROR: something failed
+}
+
+_main || _error
