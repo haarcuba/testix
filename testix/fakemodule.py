ef01594ffe384b790f8daecb5f06f17cb66f2c2b
Yoav Kleinberger <haarcuba@gmail.com>
2012-09-13 17:22:04 +0300

    bugfix: it was not possible to fake a module without faking all its parents

--- testix/fakemodule.py
+++ testix/fakemodule.py
@@ -6,17 +6,13 @@ _fakeModules = {}
 def _fakeSingleModule( name ):
 	if name in _fakeModules:
 		return
-	components = name.split( '.' )
 	_fakeModules[ name ] = fakeobject.FakeObject( name )
 	sys.modules[ name ] = _fakeModules[ name ]
+	components = name.split( '.' )
 	if len( components ) > 1:
 		parent = '.'.join( components[ :-1 ] )
 		child = components[ -1 ]
 		setattr( sys.modules[ parent ], child, _fakeModules[ name ] )
 
 def fakeModule( name ):
-	components = name.split( '.' )
-	nameComponents = []
-	while len( components ) > 0:
-		nameComponents.append( components.pop( 0 ) )
-		_fakeSingleModule( '.'.join( nameComponents ) )
+	_fakeSingleModule( name )
--- testix/startertests/startertest_fakemodule.py
+++ testix/startertests/startertest_fakemodule.py
@@ -26,5 +26,13 @@ class StarterTestFakeModule( startertestcollection.StarterTestCollection ):
 		fakeModuleUser.createSocket()
 		STS_ASSERT_IS_FAKE_OBJECT( fakeModuleUser.socket(), 'fake socket' )
 		aScenario.end()
+
+	def starter_test_Bugfix_FakeOnlyOneSubmodule( self ):
+		import fathermodule
+		fakemodule.fakeModule( 'fathermodule.firstsubmodule' )
+		from fathermodule import firstsubmodule
+		from fathermodule import secondsubmodule
+		STS_ASSERT( firstsubmodule is fakeobject.FakeObject( 'fathermodule.firstsubmodule' ) )
+		STS_ASSERT( not isinstance( secondsubmodule, fakeobject.FakeObject ) )
 		
 StarterTestFakeModule()
