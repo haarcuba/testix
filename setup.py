09850ad93856d78f3d44b1fb9c2f13919d69d4bb
Yoav Kleinberger <haarcuba@gmail.com>
2016-09-24 21:14:56 +0300

    ready for pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name = "testix",
+    packages = ["testix", "examples", "examples.tests"],
+    version = "0.1",
+    description = "Scenario Mocking framework for Python",
+    author = "Yoav Kleinberger",
+    author_email = "haarcuba@gmail.com",
+    url = "https://github.com/haarcuba/testix",
+    keywords = ["mock", "mocking", "unittest", "python", "unit testing"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Testing",
+        ]
+)
