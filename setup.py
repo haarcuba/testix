389e3554a04246cf8f5e40385e802eb6e9b5a113
Yoav Kleinberger <haarcuba@gmail.com>
2016-09-24 21:32:57 +0300

    version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = "testix",
     packages = ["testix", "examples", "examples.tests"],
-    version = "0.1",
+    version = "0.2",
     description = "Scenario Mocking framework for Python",
     author = "Yoav Kleinberger",
     author_email = "haarcuba@gmail.com",
