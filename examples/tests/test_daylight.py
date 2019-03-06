2c0470da76b5e8c68f2f1c2533c8211980dba3c8
Yoav Kleinberger <yoavk@rezilion.com>
2019-03-06 16:40:34 +0200

    fix daylight test

--- examples/tests/test_daylight.py
+++ examples/tests/test_daylight.py
@@ -1,34 +1,32 @@
-import unittest
+import pytest
 from testix.frequentlyused import *
-
-import datetime
-fakeModule( 'datetime' )
+from testix import patch_module
 
 from examples import daylight
 class FakeDay( object ):
-	def __add__( self, other ):
-		return other
+        def __add__( self, other ):
+                return other
 
-class Test_Daylight( unittest.TestCase ):
-	def test_Main( self ):
-		scenario = Scenario()
-		fakeDay = FakeDay()
-		fakeDay.hour = 12
-		scenario << \
-			Call( 'datetime.date.today' ).returns( fakeDay ) << \
-			Call( 'datetime.datetime.today' ).returns( fakeDay ) << \
-			Call( 'datetime.timedelta', IgnoreArgument() ).returns( FakeDay() )
-		nextDay = daylight.nextDaylightDate()
-		self.assertTrue( nextDay is not fakeDay )
-		scenario.end()
+class Test_Daylight:
+    @pytest.fixture
+    def module_patch( self, patch_module ):
+        patch_module( daylight, 'datetime' )
 
-	def test_EarlyInTheMorningUsesSameDate( self ):
-		scenario = Scenario()
-		fakeDay = FakeDay()
-		fakeDay.hour = 2
-		scenario << \
-			Call( 'datetime.date.today' ).returns( fakeDay ) << \
-			Call( 'datetime.datetime.today' ).returns( fakeDay )
-		nextDay = daylight.nextDaylightDate()
-		self.assertTrue( nextDay is fakeDay )
-		scenario.end()
+    def test_Main( self, module_patch ):
+        with scenario.Scenario() as s:
+            fakeDay = FakeDay()
+            fakeDay.hour = 12
+            s.datetime.date.today().returns( fakeDay )
+            s.datetime.datetime.today().returns( fakeDay )
+            s.datetime.timedelta( IgnoreArgument() ).returns( FakeDay() )
+            nextDay = daylight.nextDaylightDate()
+            assert nextDay is not fakeDay
+
+    def test_EarlyInTheMorningUsesSameDate( self, module_patch ):
+        with scenario.Scenario() as s:
+            fakeDay = FakeDay()
+            fakeDay.hour = 2
+            s.datetime.date.today() >> fakeDay
+            s.datetime.datetime.today() >> fakeDay
+            nextDay = daylight.nextDaylightDate()
+            assert nextDay is fakeDay
