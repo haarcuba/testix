854f075f7f3780ab27d53cf9f19c0769c9a9d1cb
Yoav Kleinberger <haarcuba@gmail.com>
2021-08-30 20:41:01 +0300

    version 6.0.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ tests_require = [ 'hypothesis>~4.7.19', 'pytest-asyncio' ]
 setup(
     name="testix",
     packages = ["testix",],
-    version='6.0.0rc1',
+    version='6.0.0',
     description = "Mocking framework Python with *exact* Scenarios",
     author = "Yoav Kleinberger",
     author_email = "haarcuba@gmail.com",
