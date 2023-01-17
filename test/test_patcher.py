8f466a39dc4b14ed17303a07e78460e8a32a50cc
Yoav Kleinberger <haarcuba@gmail.com>
2023-01-17 14:52:48 +0200

    failing test for issue #106

--- test/test_patcher.py
+++ test/test_patcher.py
@@ -1,6 +1,7 @@
 from testix import Fake
 from testix import Scenario
 from testix.patch_module import Patcher
+import some_module
 
 class AnObject:
     pass
@@ -49,3 +50,13 @@ def test_scenario_should_not_reset_fake_modules():
     Fake('name').length = 222
     with Scenario() as s:
         assert type(Fake('name').length) is Fake
+
+def test_bugfix_mocking_same_module_twice_raises_exception():
+    import socket
+    patcher = Patcher()
+    patcher(some_module, 'socket')
+    patcher(some_module.helper_module, 'socket')
+    assert some_module.socket is Fake('socket')
+    assert some_module.helper_module.socket is Fake('socket')
+    patcher.undo()
+    assert some_module.socket is socket
