71e6ca4e5367c7796690158c3eb51bd7471898fd
Yoav Kleinberger <haarcuba@gmail.com>
2023-05-26 20:39:23 +0300

    better pyproject

--- docs/conf.py
+++ docs/conf.py
@@ -4,8 +4,8 @@ copyright = '2022, Yoav Kleinberger'
 author = 'Yoav Kleinberger'
 
 # The full version, including alpha/beta/rc tags
-release = '10.2.1'
-version = '10.2.1'
+release = '10.3.1'
+version = '10.3.1'
 
 
 # -- General configuration ---------------------------------------------------
