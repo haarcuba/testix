904111df839fae0107b9dc707e4534771af54185
Yoav Kleinberger <haarcuba@gmail.com>
2023-05-29 17:06:13 +0300

    Document everlasting expectations

---
+++ docs/reference/test_everlasting.py
@@ -0,0 +1,22 @@
+from testix import *
+
+def test_unordered_expecation():
+    with Scenario() as s:
+        s.some_object('a')
+        s.some_object('b')
+        s.some_object('c').unordered().everlasting()
+
+        my_fake = Fake('some_object')
+        my_code(my_fake)
+
+def my_code(x):
+    x('c')
+    x('c')
+    x('c')
+
+    x('a')
+    x('c')
+    x('b')
+
+    x('c')
+    x('c')
